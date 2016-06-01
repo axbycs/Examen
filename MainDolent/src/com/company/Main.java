c660300913ae35d83381d42d4c389ae0826a255c
axbycs <emiliocasasgarcia@gmail.com>
2016-06-01 12:25:20 +0200

    Commit de la documentació

--- MainDolent/src/com/company/Main.java
+++ MainDolent/src/com/company/Main.java
@@ -3,14 +3,28 @@ package com.company;
 import java.util.Scanner;
 import java.util.Random;
 
+/**
+ * El main és la clase principal que conté el codi executable
+ * 
+ * @author Emilio Casas
+ *
+ */
 public class Main {
 
-    static protected String paraules[] = {"hola","programaciÃ³","casa"};
+    static protected String paraules[] = {"hola","programació","casa"};
     static protected int index;
-    static protected String p;
+    static protected String p = "";
+    
+    /**
+     * És el primer codi s'executa per defecte en un programa.
+     * Executa la funció novalletra mentre que els errors son menors que 5
+     * i no se ha encertat la paraula.
+     * 
+     * @param args serveix per poder presentar i llegir dades.
+     */
 
     public static void main(String[] args) {
-        System.out.println("ComenÃ§a el joc!");
+        System.out.println("Comença el joc!");
 
         Random rand = new Random();
         index = rand.nextInt(3);
@@ -31,6 +45,13 @@ public class Main {
             System.out.println(p);
         }
    }
+    
+    /**
+     * És una funció que serveix per buscar una lletra i descubrir-la.
+     * 
+     * @param nova es la lletra a buscar.
+     * @return retorna verdader o fals (si se ha trobat o no).
+     */
 
     static public boolean nova_lletra(char nova) {
         boolean trobada = false;
