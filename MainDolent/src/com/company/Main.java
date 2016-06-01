4059ed6d1dc9f130e4c45b5334543881449b8539
axbycs <emiliocasasgarcia@gmail.com>
2016-06-01 13:32:00 +0200

    Commit de la refracció

--- MainDolent/src/com/company/Main.java
+++ MainDolent/src/com/company/Main.java
@@ -11,63 +11,81 @@ import java.util.Random;
  */
 public class Main {
 
-    static protected String paraules[] = {"hola","programació","casa"};
-    static protected int index;
-    static protected String p = "";
-    
-    /**
-     * És el primer codi s'executa per defecte en un programa.
-     * Executa la funció novalletra mentre que els errors son menors que 5
-     * i no se ha encertat la paraula.
-     * 
-     * @param args serveix per poder presentar i llegir dades.
-     */
+	/**
+	 * És el primer codi s'executa per defecte en un programa. Executa la funció
+	 * nova lletra mentre que els errors son menors que 5 i no se ha encertat la
+	 * paraula.
+	 * 
+	 * @param args
+	 *            serveix per poder presentar i llegir dades.
+	 */
 
-    public static void main(String[] args) {
-        System.out.println("Comença el joc!");
+	public static void main(String[] args) {
+		Paraula paraula = new Paraula();
 
-        Random rand = new Random();
-        index = rand.nextInt(3);
-        for (int i=0; i<paraules[index].length(); i++) {
-            p += "_";
-        }
+		System.out.println("Comença el joc!");
 
-        System.out.println(p);
+		System.out.println(paraula.getP());
 
-        Scanner reader = new Scanner(System.in);
+		Scanner reader = new Scanner(System.in);
 
-        int num_errors = 0;
+		int num_errors = 0;
 
-        while (!p.equals(paraules[index]) && num_errors < 5) {
-            if (!nova_lletra(reader.next().charAt(0))) {
-                num_errors++;
-            }
-            System.out.println(p);
-        }
-   }
-    
-    /**
-     * És una funció que serveix per buscar una lletra i descubrir-la.
-     * 
-     * @param nova es la lletra a buscar.
-     * @return retorna verdader o fals (si se ha trobat o no).
-     */
-
-    static public boolean nova_lletra(char nova) {
-        boolean trobada = false;
-        String paraula_old = p;
-        p = "";
-        for (int i=0; i<paraules[index].length(); i++) {
-            if (paraules[index].charAt(i) == nova) {
-                p += paraules[index].charAt(i);
-                trobada = true;
-            }
-            else {
-                p += paraula_old.charAt(i);
-            }
-        }
-        return trobada;
-    }
-
+		while (!paraula.getP().equals(paraula.getParaula()) && num_errors < 5) {
+			if (!paraula.buscar_lletra(reader.next().charAt(0))) {
+				num_errors++;
+			}
+			System.out.println(paraula.getP());
+		}
+		reader.close();
+	}
 }
 
+class Paraula {
+	String paraules[] = { "hola", "programació", "casa" };
+	String p = "";
+	int index;
+
+	public Paraula() {
+		Random rand = new Random();
+		index = rand.nextInt(3);
+		for (int i = 0; i < paraules[index].length(); i++) {
+			p += "_";
+		}
+	}
+
+	public String getParaula() {
+		return paraules[index];
+	}
+
+	public String getP() {
+		return p;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	/**
+	 * És una funció que serveix per buscar una lletra i descubrir-la.
+	 * 
+	 * @param nova
+	 *            es la lletra a buscar.
+	 * @return retorna verdader o fals (si se ha trobat o no).
+	 */
+	public boolean buscar_lletra(char nova) {
+		boolean trobada = false;
+		String paraula_old = p;
+		p = "";
+		for (int i = 0; i < paraules[index].length(); i++) {
+			if (paraules[index].charAt(i) == nova) {
+				p += paraules[index].charAt(i);
+				trobada = true;
+			} else {
+				p += paraula_old.charAt(i);
+			}
+		}
+		return trobada;
+	}
+
+}
---
+++ MainDolent/src/com/company/test_nova_lletra.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test_nova_lletra {
+
+	@Test
+	public void test() {
+		
+		assertEquals(true, Main.nova_lletra('h'));
+	}
+
+}
